f1179b9a2ea27a4d58f1f065b2d49154693b78e4
Brindia Joseph <brindiatyjuana@gmail.com>
2022-04-12 15:35:19 -0500

    commit tree 10cbfe071a55874a1e658d045191760aea2c7a6a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [// each objext has 2 properties year and result
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  //takes 1 argument
+// superbowlWin = (record) => {
+//     //use find to test each obj to see if result = W (logical operaters)
+//    result = record.find ( record => record.result === "W" )// should return the year when the win occured 
+//    return !!result ? result.year : undefined;// returns undefined if no win is found 
+// }
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
